f39312da9f8777276b1ff49e6a9e55fa812041db
Andrey Nazarenko <merdisst@gmail.com>
2025-03-16 16:10:23 +0300

    sh

--- OP-Premium/gitlabInfo.js
+++ OP-Premium/gitlabInfo.js
@@ -1,11 +1,14 @@
 const svg = {
     merged: `<svg _ngcontent-ng-c1779735674="" op-gitlab-merge-request-merged-icon="" size="small" role="img" fill="currentColor" id="" tabindex="-1" aria-label="" aria-labelledby="" aria-hidden="true" focusable="false" viewBox="0 0 16 16" height="16" width="16" class="octicon" style="display: inline-block; overflow: visible; user-select: none; vertical-align: text-bottom;"><!----><path d="M5.456 5.81v.001A4.253 4.253 0 0 0 9.12 8.733a2.5 2.5 0 1 1-.01 1.504 5.734 5.734 0 0 1-3.86-1.864v1.741a2.501 2.501 0 1 1-1.5 0V5.886a2.5 2.5 0 1 1 1.706-.076ZM11.5 10.5a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm-6 2a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm0-9a1 1 0 1 0-2 0 1 1 0 0 0 2 0Z"></path><!----></svg>`,
-    open: `<svg _ngcontent-ng-c1779735674="" op-gitlab-merge-request-ready-icon="" size="small" role="img" fill="currentColor" id="" tabindex="-1" aria-label="" aria-labelledby="" aria-hidden="true" focusable="false" viewBox="0 0 16 16" height="16" width="16" class="octicon" style="display: inline-block; overflow: visible; user-select: none; vertical-align: text-bottom;"><!----><path d="M6 12.5a2.5 2.5 0 1 1-3.25-2.386V5.886a2.501 2.501 0 1 1 1.5 0v4.228A2.501 2.501 0 0 1 6 12.5Zm4.34-11.28a.75.75 0 0 1 0 1.06l-.47.47h.63a2.75 2.75 0 0 1 2.75 2.75v4.614a2.501 2.501 0 1 1-1.5 0V5.5c0-.69-.56-1.25-1.25-1.25h-.63l.47.47a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L7.53 4.03 7 3.5l.53-.53 1.75-1.75a.75.75 0 0 1 1.06 0ZM13.5 12.5a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm-9 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm0-9a1 1 0 1 0-2 0 1 1 0 0 0 2 0Z"></path><!----></svg><!----><path d="M5.456 5.81v.001A4.253 4.253 0 0 0 9.12 8.733a2.5 2.5 0 1 1-.01 1.504 5.734 5.734 0 0 1-3.86-1.864v1.741a2.501 2.501 0 1 1-1.5 0V5.886a2.5 2.5 0 1 1 1.706-.076ZM11.5 10.5a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm-6 2a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm0-9a1 1 0 1 0-2 0 1 1 0 0 0 2 0Z"></path><!----></svg>`
+    open: `<svg _ngcontent-ng-c1779735674="" op-gitlab-merge-request-ready-icon="" size="small" role="img" fill="currentColor" id="" tabindex="-1" aria-label="" aria-labelledby="" aria-hidden="true" focusable="false" viewBox="0 0 16 16" height="16" width="16" class="octicon" style="display: inline-block; overflow: visible; user-select: none; vertical-align: text-bottom;"><!----><path d="M6 12.5a2.5 2.5 0 1 1-3.25-2.386V5.886a2.501 2.501 0 1 1 1.5 0v4.228A2.501 2.501 0 0 1 6 12.5Zm4.34-11.28a.75.75 0 0 1 0 1.06l-.47.47h.63a2.75 2.75 0 0 1 2.75 2.75v4.614a2.501 2.501 0 1 1-1.5 0V5.5c0-.69-.56-1.25-1.25-1.25h-.63l.47.47a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L7.53 4.03 7 3.5l.53-.53 1.75-1.75a.75.75 0 0 1 1.06 0ZM13.5 12.5a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm-9 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm0-9a1 1 0 1 0-2 0 1 1 0 0 0 2 0Z"></path><!----></svg><!----><path d="M5.456 5.81v.001A4.253 4.253 0 0 0 9.12 8.733a2.5 2.5 0 1 1-.01 1.504 5.734 5.734 0 0 1-3.86-1.864v1.741a2.501 2.501 0 1 1-1.5 0V5.886a2.5 2.5 0 1 1 1.706-.076ZM11.5 10.5a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm-6 2a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm0-9a1 1 0 1 0-2 0 1 1 0 0 0 2 0Z"></path><!----></svg>`,
+    gl: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 88.6 85.33"><defs><style>.axdzxcd{fill:#e24329;}.axdzxcdazxc{fill:#fc6d26;}.axdzxcdazxcas{fill:#fca326;}</style></defs><title>Gitlab</title><g id="Слой_2" data-name="Слой 2"><g id="final"><path class="axdzxcd" d="M87.12,33.77,87,33.45,75,2a3.18,3.18,0,0,0-6,.32L60.79,27.24h-33L19.68,2.32a3.19,3.19,0,0,0-6-.32L1.6,33.46l-.12.31A22.39,22.39,0,0,0,8.91,59.65l0,0,.11.08L27.4,73.51l9.1,6.88L42,84.57a3.74,3.74,0,0,0,4.51,0l5.52-4.18,9.11-6.88L79.64,59.68l0,0A22.36,22.36,0,0,0,87.12,33.77Z"/><path class="axdzxcdazxc" d="M83.11,34.61,83,34.32a36.84,36.84,0,0,0-14.69,6.61l-24,18.14L59.59,70.62,76.34,58.08l0,0A20.27,20.27,0,0,0,83.11,34.61Z"/><path class="axdzxcdazxcas" d="M29,70.62l8.25,6.23,5,3.79a3.41,3.41,0,0,0,4.09,0l5-3.79,8.24-6.23L44.3,59.07Z"/><path class="axdzxcdazxc" d="M20.3,40.93A36.81,36.81,0,0,0,5.6,34.32l-.11.29a20.3,20.3,0,0,0,6.74,23.45l0,0,.09.07L29,70.62,44.27,59.07l-24-18.14Z"/></g></g></svg><!----><path d="M6 12.5a2.5 2.5 0 1 1-3.25-2.386V5.886a2.501 2.501 0 1 1 1.5 0v4.228A2.501 2.501 0 0 1 6 12.5Zm4.34-11.28a.75.75 0 0 1 0 1.06l-.47.47h.63a2.75 2.75 0 0 1 2.75 2.75v4.614a2.501 2.501 0 1 1-1.5 0V5.5c0-.69-.56-1.25-1.25-1.25h-.63l.47.47a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L7.53 4.03 7 3.5l.53-.53 1.75-1.75a.75.75 0 0 1 1.06 0ZM13.5 12.5a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm-9 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm0-9a1 1 0 1 0-2 0 1 1 0 0 0 2 0Z"></path><!----></svg><!----><path d="M5.456 5.81v.001A4.253 4.253 0 0 0 9.12 8.733a2.5 2.5 0 1 1-.01 1.504 5.734 5.734 0 0 1-3.86-1.864v1.741a2.501 2.501 0 1 1-1.5 0V5.886a2.5 2.5 0 1 1 1.706-.076ZM11.5 10.5a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm-6 2a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm0-9a1 1 0 1 0-2 0 1 1 0 0 0 2 0Z"></path><!----></svg>`,
 }
 
-const getGLIcon = (type = 'open') => {
+const getGLIcon = (type = 'open', width, height) => {
     const ic = document.createElement('div')
     ic.innerHTML = svg[type];
+    if (width) ic.style.width = `${width}px`
+    if (height || width) ic.style.height = `${height || width}px`
     return ic
 }
 
@@ -27,23 +30,30 @@ const placeGitlabLinks = async () => {
     const taskNumber = document.querySelector('.premium-taskNumberSpan')?.innerHTML;
     if (taskNumber) {
         const mrList = await getGitlabList(taskNumber);
-        const toolbarButtonsRow = document.getElementById('premium-buttonsRow');
+        if (mrList.length) {
+            const toolbarButtonsRow = document.getElementById('premium-buttonsRow');
 
-        const container = document.createElement('div')
-        toolbarButtonsRow.append(container);
-        container.setAttribute('id', 'gitlabInfoContainer');
+            const container = document.createElement('div')
+            toolbarButtonsRow.append(container);
+            container.setAttribute('id', 'gitlabInfoContainer');
 
-        mrList.forEach(({ repository, htmlUrl, merged, title, number }) => {
-            const mrBlock = document.createElement('div')
-            container.append(mrBlock);
-            mrBlock.classList.add('mr-block');
-            mrBlock.title = `Открыть №${number} "${title}"`;
-            mrBlock.innerHTML = repository;
-            mrBlock.prepend(getGLIcon(merged ? 'merged' : 'open'));
-            if (merged) mrBlock.classList.add('merged');
-            mrBlock.onclick = () => {
-                window.open(htmlUrl, '_blank');
-            };
-        })
+            const labelContainer = document.createElement('div')
+            container.append(labelContainer);
+            labelContainer.setAttribute('id', 'labelContainer');
+            labelContainer.append(getGLIcon('gl', 16));
+
+            mrList.forEach(({repository, htmlUrl, merged, title, number}) => {
+                const mrBlock = document.createElement('div')
+                labelContainer.append(mrBlock);
+                mrBlock.classList.add('mr-block');
+                mrBlock.title = `Открыть №${number} "${title}"`;
+                mrBlock.innerHTML = repository;
+                mrBlock.prepend(getGLIcon(merged ? 'merged' : 'open'));
+                if (merged) mrBlock.classList.add('merged');
+                mrBlock.onclick = () => {
+                    window.open(htmlUrl, '_blank');
+                };
+            })
+        }
     }
 }
